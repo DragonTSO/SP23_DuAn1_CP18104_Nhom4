4fc349f7b1e183b3fcd78f4de0019c59cb180728
DragonDirt <hoangdoan7c@gmail.com>
2023-03-15 14:27:27 +0700

    first commit

---
+++ app/src/main/java/com/example/sp23_duan1_cp18104_nhom4/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.sp23_duan1_cp18104_nhom4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //asd
+}
